7a3c16aa4f21c1ea419982be1bad5267d5af0adb
Silver-Snail <jlc29945@gmail.com>
2024-06-07 11:00:02 -0400

    Create tampermonkey.js

---
+++ tampermonkey.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Flightradar24 Add Remover
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  Remove adds from flightradar24.
+// @author       You
+// @match        https://www.flightradar24.com/*
+// @icon         https://www.flightradar24.com/static/favicons/favicon.svg
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let checkCondition = setInterval(function() {
+        if (document.querySelector("#app > div > div > div.flex.min-h-full.w-0.flex-1.flex-col.overflow-hidden.bg-white > main > div > div > div > div.absolute.flex.top-12.rounded-xl.mt-4.ml-4.z-40.lg\\:top-0.w-84.overflow-hidden.shadow-default > div > main > div > div.my-4.flex.items-center.justify-center") != null)
+        {
+            document.querySelector("#app > div > div > div.flex.min-h-full.w-0.flex-1.flex-col.overflow-hidden.bg-white > main > div > div > div > div.absolute.flex.top-12.rounded-xl.mt-4.ml-4.z-40.lg\\:top-0.w-84.overflow-hidden.shadow-default > div > main > div > div.my-4.flex.items-center.justify-center").remove();
+        }
+        if (document.querySelector("#primisAdContainer") != null)
+        {
+           document.querySelector("#primisAdContainer").remove();
+        }
+}, 1000);
+})();
